f3a30ce740432e635b7928617c13f70f4e8ce66a
gajendrasingh <gajendra.singh@preciousinfosystem.com>
2023-06-05 18:21:47 +0530

    tic tac toe task completed

---
+++ tic-tac-toe/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+// import TicTacToe from './tic_tac_toe/TicTacToe';
+import Tictactoe from './componets/Tictactoe';
+
+function App() {
+  return (
+    <div className="App">
+      <Tictactoe/>
+    </div>
+  );
+}
+
+export default App;
